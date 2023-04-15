14df5d0b2d628a87a4967d13697f4a0771597f10
Nursat Zeinolla <kznursat@gmail.com>
2023-04-15 11:57:03 +0600

    Add project files.

---
+++ UmagAPI/Controllers/SalesController.cs
@@ -0,0 +1,4 @@
+﻿namespace UmagAPI.Controllers {
+    public class SalesController {
+    }
+}
---
+++ UmagAPI/Controllers/SupplyController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UmagAPI.Controllers {
+    [ApiController]
+    [Route("api/supplies")]
+    public class SupplyController : ControllerBase {
+        [HttpGet]
+        public async Task<IActionResult> Get([FromQuery] string barcode, DateTime fromTime, DateTime toTime) {
+            // retriev from db
+            // business logic
+            return new JsonResult("Hello");
+        }
+    }
+}
